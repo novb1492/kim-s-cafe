5cee1cbee74fa52ae0d4d29a1c654bc2b7eaade5
novb1492 <novb1492@naver.com>
2021-05-25 23:05:36 +0900

    radio중복선택 방지는 name값을 똑같이 주면 막힌다

--- src/main/java/com/example/kim_s_cafe/config/security.java
+++ src/main/java/com/example/kim_s_cafe/config/security.java
@@ -35,8 +35,10 @@ public class security extends WebSecurityConfigurerAdapter {
        http
          .csrf().disable()///ajax사용하기 위해 토큰은 나중에
          .authorizeRequests()////요청이발생
-         .antMatchers("/","auth/**")////이 링크들은
+         .antMatchers("/","/auth/**")////이 링크들은
          .permitAll()///허용한다
+         //.anyRequest()
+         //.authenticated()
         .and()
             .formLogin()////로그인 발생시
             .loginPage("/auth/loginpage")///로그인페이지 지정
