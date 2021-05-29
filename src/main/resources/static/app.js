eb3da4613f59702c2621f7a1707804994b049d0f
novb1492 <novb1492@naver.com>
2021-05-29 14:36:57 +0900

    탁자그리기 시작 p5js 이용예정

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,12 @@
+function setup() {
+    var framerate=60;
+    frameRate(framerate);
+    let canvas=createCanvas(displayWidth,windowHeight);
+    canvas.parent('canvas');
+ 
+  }
+
+function draw() 
+{  
+     background('gray'); 
+}
